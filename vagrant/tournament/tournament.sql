91e4ff0f60e3fe4fa94b42dd1bcc7557f93fede1
Seananigans <sph99@comcast.net>
2016-08-25 22:19:59 -0400

    feat: Improve normalization for database
    feat: Improve normalization for database
    
    Data is more easily retrieved, manipulated, and understood in separate
    tables and views.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,8 +6,31 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE IF NOT EXISTS players (
+	id SERIAL PRIMARY KEY,
+	name TEXT);
 
-CREATE TABLE tournament (id SERIAL PRIMARY KEY,
-	player TEXT,
-	wins INT,
-	matches INT);
+CREATE TABLE IF NOT EXISTS matches (
+	id SERIAL PRIMARY KEY,
+	winner INTEGER REFERENCES players (id),
+	loser INTEGER REFERENCES players (id));
+
+CREATE VIEW wins AS 
+	SELECT players.id, COUNT(matches.winner) as ws
+	FROM players 
+	LEFT JOIN matches 
+	ON players.id=matches.winner 
+	GROUP BY players.id;
+
+CREATE VIEW total AS 
+	SELECT players.id, COUNT(matches.winner) as tot
+	FROM players 
+	LEFT JOIN matches 
+	ON players.id=matches.winner OR players.id=matches.loser
+	GROUP BY players.id;
+
+CREATE VIEW standings AS
+	SELECT players.id, players.name, wins.ws as wins, total.tot as matches
+	FROM players, wins, total
+	WHERE players.id = wins.id and players.id = total.id
+	ORDER BY wins DESC;
