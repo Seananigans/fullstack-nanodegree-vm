235c3521b9389d70c64aaaacb8b86120c248b060
Seananigans <sph99@comcast.net>
2016-08-29 23:35:17 -0400

    chore: Remove whitespace

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -19,16 +19,16 @@ class MenuItem(Base):
 	__tablename__ = "menu_item"
 	name = Column(String(80), nullable=False)
 	id = Column(Integer, primary_key=True)
-	
+
 	course = Column(String(250))
-	
+
 	description = Column(String(250))
-	
+
 	price = Column(String(8))
-	
+
 	restaurant_id = Column(
 		Integer, ForeignKey('restaurant.id'))
-	
+
 	restaurant = relationship(Restaurant)
 
 ### Insert at the end of file ###
